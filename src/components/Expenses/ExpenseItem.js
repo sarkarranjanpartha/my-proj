5b9a26e75bbd3a6b124f3e65149053360db9e0d5
Partha Sarkar <sarkarranjanpartha@gmail.com>
2023-02-24 23:03:41 +0530

    My first git

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    console.log("Updated!");
-    setTitle("Updated!");
-  };
+  // const [title, setTitle] = useState(props.title);
+  // const clickHandler = () => {
+  //   console.log("Updated!");
+  //   setTitle("Updated!");
+  // };
 
   // const expenseDate = new Date(2021, 2, 28);
   // const expenseTitle = "Car Insurance";
@@ -18,10 +18,10 @@ function ExpenseItem(props) {
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
 }
