dba8a6b1b3a26eedf9a2724d6a823f5f69cca415
Partha Sarkar <sarkarranjanpartha@gmail.com>
2023-02-24 23:43:52 +0530

    My 2nd git

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,16 +4,6 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  // const [title, setTitle] = useState(props.title);
-  // const clickHandler = () => {
-  //   console.log("Updated!");
-  //   setTitle("Updated!");
-  // };
-
-  // const expenseDate = new Date(2021, 2, 28);
-  // const expenseTitle = "Car Insurance";
-  // const expenseAmount = 294.67;
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -21,7 +11,6 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
 }
