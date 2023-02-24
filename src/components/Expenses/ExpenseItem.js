6fece407700c31f3a00fb3261588b867dddef7d3
Partha Sarkar <sarkarranjanpartha@gmail.com>
2023-02-24 15:40:15 +0530

    My fit git

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    console.log("Updated!");
+    setTitle("Updated!");
+  };
+
+  // const expenseDate = new Date(2021, 2, 28);
+  // const expenseTitle = "Car Insurance";
+  // const expenseAmount = 294.67;
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
